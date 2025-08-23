7f5de68c5ee8b85f2c25f679d5746cdb08c56134
binod rai <br163406@gmail.com>
2025-08-23 23:36:28 +0545

    login page is complete

--- frontend/src/MainRoutes/routes.jsx
+++ frontend/src/MainRoutes/routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import  Register  from "../Pages/register"
 import  Home  from "../Pages/home"
+import Login from "../Pages/login"
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
