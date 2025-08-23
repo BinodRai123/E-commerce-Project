ab8d6da396a2677025dd829b59e4e4401a964d96
binod rai <br163406@gmail.com>
2025-08-24 00:57:11 +0545

    auth wrapper for navlink

---
+++ frontend/src/MainRoutes/authWrapper.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const AuthWrapper = (props) => {
+  const { user } = useSelector((state) => state.userReducer);
+  return user ? props.children : <Navigate to="/register" />;
+};
+
+export default AuthWrapper;
--- frontend/src/MainRoutes/routes.jsx
+++ frontend/src/MainRoutes/routes.jsx
@@ -4,15 +4,16 @@ import  Home  from "../Pages/home"
 import Login from "../Pages/login"
 import Products from "../Pages/products"
 import LogOut from "../Pages/logOut"
+import AuthWrapper from "./authWrapper"
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/products" element={<Products />} />
+      <Route path="/products" element={<AuthWrapper><Products /></AuthWrapper>} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/logout" element={<LogOut />} />
+      <Route path="/logout" element={<AuthWrapper><LogOut /></AuthWrapper>} />
     </Routes>
   )
 }
