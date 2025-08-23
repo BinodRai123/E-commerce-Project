9d705e78f5c3f6cfe06118020ba4de0221996839
binod rai <br163406@gmail.com>
2025-08-23 23:43:10 +0545

    login form complete

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -1,9 +1,29 @@
-const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
-export default Login
+const Login = () => {
+    
+    const { register, handleSubmit, reset } = useForm();
+      const dispatch = useDispatch();
+    
+      const handleRegister = (data) => {
+        dispatch(asyncLoginUser(data));
+        reset();
+      };
+  return (
+    <>
+      <form className="flex flex-col" onSubmit={handleSubmit(handleRegister)}>
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="abc@gmail.com"
+        />
+        <input {...register("password")} type="password" placeholder="*****" />
+
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/Pages/register.jsx
+++ frontend/src/Pages/register.jsx
@@ -2,7 +2,7 @@ import { nanoid } from "@reduxjs/toolkit";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { asyncRegisterUser } from "../Store/Actions/userAction";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const { register, handleSubmit, reset } = useForm();
@@ -15,17 +15,29 @@ const Register = () => {
     reset();
   };
 
-  return <>
-    <form className="flex flex-col" onSubmit={handleSubmit(handleRegister)}>
-      <input {...register("name")} type="text" placeholder="Enter your name" />
-      <input {...register("email")} type="email" placeholder="abc@gmail.com" />
-      <input {...register("password")} type="password" placeholder="*****" />
+  return (
+    <>
+      <form className="flex flex-col" onSubmit={handleSubmit(handleRegister)}>
+        <input
+          {...register("name")}
+          type="text"
+          placeholder="Enter your name"
+        />
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="abc@gmail.com"
+        />
+        <input {...register("password")} type="password" placeholder="*****" />
 
-      <input type="submit" />
-    </form>
+        <input type="submit" />
+      </form>
 
-    <p>Already have an Account: <Link to={"/login"}>Login</Link></p>
-  </>
+      <p>
+        Already have an Account: <Link to={"/login"}>Login</Link>
+      </p>
+    </>
+  );
 };
 
 export default Register;
