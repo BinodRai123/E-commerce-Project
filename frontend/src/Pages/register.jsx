408bc6a8d50060e0563c81b81509e07b153a846a
binod rai <br163406@gmail.com>
2025-08-23 23:40:11 +0545

    shift to login page complete

--- frontend/src/Pages/register.jsx
+++ frontend/src/Pages/register.jsx
@@ -2,6 +2,7 @@ import { nanoid } from "@reduxjs/toolkit";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { asyncRegisterUser } from "../Store/Actions/userAction";
+import {Link} from "react-router-dom"
 
 const Register = () => {
   const { register, handleSubmit, reset } = useForm();
@@ -9,20 +10,22 @@ const Register = () => {
 
   const handleRegister = (data) => {
     data.id = nanoid();
-    data.carts = [{productId: null, count: 0}]
+    data.carts = [{ productId: null, count: 0 }];
     dispatch(asyncRegisterUser(data));
     reset();
   };
 
-  return (
-    <form onSubmit={handleSubmit(handleRegister)}>
+  return <>
+    <form className="flex flex-col" onSubmit={handleSubmit(handleRegister)}>
       <input {...register("name")} type="text" placeholder="Enter your name" />
       <input {...register("email")} type="email" placeholder="abc@gmail.com" />
       <input {...register("password")} type="password" placeholder="*****" />
 
       <input type="submit" />
     </form>
-  );
+
+    <p>Already have an Account: <Link to={"/login"}>Login</Link></p>
+  </>
 };
 
 export default Register;
