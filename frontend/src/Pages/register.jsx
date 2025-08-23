3951ff19abc2bbcc68aa05ef0f0e52ad5169200e
binod rai <br163406@gmail.com>
2025-08-23 23:03:33 +0545

    make register form

--- frontend/src/Pages/register.jsx
+++ frontend/src/Pages/register.jsx
@@ -1,8 +1,23 @@
+import { nanoid } from "@reduxjs/toolkit";
+import {useForm} from "react-hook-form"
+
 const Register = () => {
+  const {register, handleSubmit, reset} = useForm();
+
+  const handleRegister = (data) => {
+    data.id = nanoid();
+    reset();
+  }
+
   return (
-    <div>
-      register
-    </div>
+    <form onSubmit={handleSubmit(handleRegister)}>
+      <input {...register("name")} type="text" placeholder="Enter your name" />
+      <input {...register("email")} type="email" placeholder="abc@gmail.com" />
+      <input {...register("password")} type="password" placeholder="*****" />
+
+      <input type="submit" />
+
+    </form>
   )
 }
 
